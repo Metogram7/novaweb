2d0e1421b432ecca5934bd7ef9c8af90f5b5fcdf
Metogram7 <metehanakkaya30@gmail.com>
2025-11-15 20:14:33 +0300

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,21 +1,25 @@
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.5.0/firebase-messaging.js');
 
-firebase.initializeApp({
-    apiKey: "AIzaSyDqQAHCB2MizgN5Id5Ezfr3UPE1P3UlSS8",
-    authDomain: "nova-329c7.firebaseapp.com",
-    projectId: "nova-329c7",
-    storageBucket: "nova-329c7.appspot.com",
-    messagingSenderId: "284547967902",
-    appId: "1:284547967902:web:7dd2e64d1a643a30e5c48f"
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyDqQAHCB2MizgN5Id5Ezfr3UPE1P3UlSS8",
+  authDomain: "nova-329c7.firebaseapp.com",
+  projectId: "nova-329c7",
+  storageBucket: "nova-329c7.firebasestorage.app",
+  messagingSenderId: "284547967902",
+  appId: "1:284547967902:web:7dd2e64d1a643a30e5c48f"
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-    console.log('[Service Worker] Background Message:', payload);
-    self.registration.showNotification(payload.notification.title, {
-        body: payload.notification.body,
-        icon: ''
-    });
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[SW] Background mesajı:', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
